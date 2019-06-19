4fe53f0a7628c8449e3427b27cea735690002ab7
Caleb Kim <Calebk@trojanonline.local>
2019-06-19 12:07:07 -0700

    Created Unit Test to Explore how Value and Ref work

--- test/GradeBook.Tests/BookTests.cs
+++ test/GradeBook.Tests/BookTests.cs
@@ -6,8 +6,8 @@ namespace GradeBook.Tests
 {
     public class BookTests
     {
-        [Fact] // This is called an attribute in C#. This of an attribute as a little piece of data attached to the symbol that follows it. So "Fact" is a little piece of data that is attached to the method Test1(). When xUnit loads up your test project and tries to find the test inside and execute them, and telling you whether they passed or failed, it will look for methods that have this [Fact] attribute attached. 
-        public void Test1()
+        [Fact] 
+        public void BookCalculatesStats()
         {
             //arrange
             var book = new Book("");
--- test/GradeBook.Tests/TypeTests.cs
+++ test/GradeBook.Tests/TypeTests.cs
@@ -6,22 +6,72 @@ namespace GradeBook.Tests
 {
     public class TypeTests
     {
-        [Fact] // This is called an attribute in C#. This of an attribute as a little piece of data attached to the symbol that follows it. So "Fact" is a little piece of data that is attached to the method Test1(). When xUnit loads up your test project and tries to find the test inside and execute them, and telling you whether they passed or failed, it will look for methods that have this [Fact] attribute attached. 
-        public void GetBookRetunsDifferentObjects()
+        [Fact]
+        public void ValueTypesAlsoPassByValue()
         {
-            //arrange
+            var x = GetInt();
+            SetInt(x);
+            Assert.Equal(3,x);
+        }
 
+        private void SetInt(int x)
+        {
+            x = 42;
+        }
+
+        private int GetInt()
+        {
+            return 3;
+        }
+
+        [Fact]
+        public void CanSetNameFromReference()
+        {
             //act
             var book1 = GetBook("Book 1");
-            var book2 = GetBook("Book 2");
+            SetName(book1, "New Name");
+
+            // assert
+            Assert.Equal("New Name", book1.Name);
+
+
+        }
+        private void SetName(Book book, string name)
+        {   
+            book.Name = name;
+        }
+        [Fact]
+        public void CSharpIsPassByValue()
+        {
+            //act
+            var book1 = GetBook("Book 1");
+            GetBookSetName(book1, "New Name");
 
             // assert
             Assert.Equal("Book 1", book1.Name);
-            Assert.Equal("Book 2", book2.Name);
-            Assert.NotSame(book1,book2);
+        }   
+        private void GetBookSetName(Book book, string name)
+        {   
+            book = new Book(name);
+        }
+        [Fact]
+        public void CSharpCanPassByRef()
+        {
+            //act
+            var book1 = GetBook("Book 1");
+            GetBookSetName(ref book1, "New Name");
+
+            // assert
+            Assert.Equal("New Name", book1.Name);
+
 
         }
+        private void GetBookSetName(ref Book book, string name) // What is passed instead of the memory address is a copy of the memory location of where that object is stored. 
+        {   
+            book = new Book(name);
+        }
 
+        [Fact]
         public void TwoVarsCanReferenceSameObject()
         {
             //arrange
@@ -34,9 +84,14 @@ namespace GradeBook.Tests
             Assert.Same(book1,book2);
 
         }
+
+        // [Fact]
+
         Book GetBook(string name) 
         {
             return new Book(name);
         }
+
+
     }
 }
